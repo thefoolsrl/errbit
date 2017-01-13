7cc7b0f374772348febd8dd0e46f040dc2595947
Guido Scicolone <g.scicolone@thefool.it>
2017-01-13 10:40:14 +0100

    fix default env for errbit

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ preload_app!
 
 rackup DefaultRackup
 port ENV['PORT'] || 8080
-environment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || 'production'
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
